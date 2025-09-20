ef14f05f20aff9942180b244db5afaed7e94521d
kaito <kaito013126@gmail.com>
2025-09-20 15:33:22 +0000

    ログインページを追加し、URL設定を更新

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path , include
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('webimage.urls')), # webimage
+    path('', views.login, name='login'), #ログイン
+    path('image/', include('webimage.urls')), # webimage
 ]
